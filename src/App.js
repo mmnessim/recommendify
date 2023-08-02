e2c570aacb0164b02abf1005e9bf3d1e728bac75
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-08-02 09:24:35 -0400

    login and logout functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/', {
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setToken(data.accessToken);
+        setProfile(data.profile);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  function handleLogout() {
+    fetch('http://localhost:3001/logout', {
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setToken(null);
+        setProfile(null);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button><a href='http://localhost:3001/auth/spotify'>Click Me</a></button>
+      <button onClick={handleLogout}>Logout</button>
+      { profile &&
+      <div>
+        <h1>{profile.displayName}</h1>
+        <img src={profile.photos[0]} alt="profile pic" />
+      </div>
+       }
     </div>
   );
 }
