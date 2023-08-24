4eaaea2e07ae8753e3327ede0059d182c165bd90
Mounir Nessim <mnessim@ad.wcpss.net>
2023-08-24 12:50:21 -0400

    enter playlist name

--- src/components/createPlaylist.js
+++ src/components/createPlaylist.js
@@ -3,8 +3,10 @@ import ShowPlaylist from './showPlaylist';
 
 export default function CreatePlaylist(props) {
     const profile = props.profile;
-    const [playlistName, setPlaylistName] = useState('test');
+    const [playlistName, setPlaylistName] = useState(null);
+    const [playlistDescription, setPlaylistDescription] = useState(null); // optional
     const [playlistID, setPlaylistID] = useState(null);
+    const [error, setError] = useState(null);
 
     function createPlaylist() {
         fetch('https://api.spotify.com/v1/users/' + profile.id + '/playlists', {
@@ -14,13 +16,17 @@ export default function CreatePlaylist(props) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                name: playlistName
+                name: playlistName,
+                "description": playlistDescription // Not working for some reason
             })
         })
             .then(res => res.json())
             .then(data => {
                 console.log(data);
                 setPlaylistID(data.id);
+                if (data.error) {
+                    setError(data.error.message);
+                }
             })
             .catch(err => console.log(err));
     }
@@ -28,8 +34,16 @@ export default function CreatePlaylist(props) {
     return (
         <div>
             <h1>Create playlist</h1>
-            <p>{profile.id}</p>
+            <input type="text" placeholder="Enter a playlist name" onChange={(e) => setPlaylistName(e.target.value)} />
+            <br />
+            <input type="text" placeholder="Enter a playlist description" onChange={(e) => setPlaylistDescription(e.target.value)} />
+            <br />
+
             <button onClick={createPlaylist}>Create a playlist</button>
+            {
+                error &&
+                <p>Error: {error}</p>
+            }
             { playlistID &&
             <div>
                 <p>Playlist ID: {playlistID}</p>
