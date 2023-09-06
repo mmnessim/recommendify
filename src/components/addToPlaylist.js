33554f0e1d28586d51f429640dcf7a41b2dd2677
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-09-06 18:05:30 -0400

    get playlist ID working

--- src/components/addToPlaylist.js
+++ src/components/addToPlaylist.js
@@ -4,13 +4,14 @@ import SelectPlaylist from './selectPlaylist';
 export default function AddToPlaylist(props) {
     const [playlist, setPlaylist] = useState(null);
     const [display, setDisplay] = useState(null);
-    const [selected, setSelected] = useState(null); 
+    const [selected, setSelected] = useState(null);
     const [playlistID, setPlaylistID] = useState(null);
 
     function handleClick(index) {
         console.log(index)
         //setPlaylistID(playlist[index].id);
-        console.log("playlist" + playlist)
+        console.log(playlist.items[index])
+        setPlaylistID(playlist.items[index].id);
         setSelected(true);
     }
 
@@ -23,15 +24,16 @@ export default function AddToPlaylist(props) {
             .then(res => res.json())
             .then(data => {
                 console.log("Data" + data.items);
-                setPlaylist(data.items);
-                setDisplay(data.items.map((item, index) => {
+                setPlaylist(data);
+                /*
+                setDisplay(playlist.items.map((item, index) => {
                     return (
                         <div key={index} className='playlists'>
                             <a className='' href={item.external_urls.spotify} target="_blank" rel="noreferrer">{item.name}   </a>
                             <button className='btn' onClick={() => {handleClick(index)}}>+</button>
                         </div>
                     )
-                }));
+                }));*/
             })
             .catch(err => console.log(err));
     }
@@ -39,6 +41,19 @@ export default function AddToPlaylist(props) {
     useEffect(() => {
         getPlaylist();
     }, []);
+
+    useEffect(() => {
+        if (playlist) {
+        setDisplay(playlist.items.map((item, index) => {
+            return (
+                <div key={index} className='playlists'>
+                    <a className='' href={item.external_urls.spotify} target="_blank" rel="noreferrer">{item.name}   </a>
+                    <button className='btn' onClick={() => {handleClick(index)}}>+</button>
+                </div>
+            )
+        }));
+    }
+    }, [playlist]);
 
 
     return (
@@ -49,7 +64,7 @@ export default function AddToPlaylist(props) {
             {selected &&
             <div>
                 <button onClick={() => setSelected(false)}>Back</button>
-                <SelectPlaylist />
+                <SelectPlaylist token={props.token} profile={props.profile} playlistID={playlistID}/>
             </div>
             }
         </div>
--- src/components/selectPlaylist.js
+++ src/components/selectPlaylist.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export default function SelectPlaylist(props) {
+    const [playlist, setPlaylist] = useState(null);
+    const [display, setDisplay] = useState(null);
+
+    function getPlaylist() {
+        fetch(`https://api.spotify.com/v1/playlists/${props.playlistID}`, {
+            headers: {
+                Authorization: 'Bearer ' + props.token
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setPlaylist(data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        getPlaylist();
+    }, []);
+
+    useEffect(() => {
+        if (playlist) {
+            setDisplay(playlist.tracks.items.map((item, index) => {
+                return (
+                    <div key={index} className='playlists'>
+                        something
+                    </div>
+                )
+            }));
+        }
+    }, [playlist]);
+
+
+
     return(
         <div>
             <p>Playlist</p>
