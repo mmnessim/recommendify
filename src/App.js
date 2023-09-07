e422ee09b8f11fa5ce97a7061b090be0a6a638a6
Mounir Nessim <mnessim@ad.wcpss.net>
2023-09-07 08:08:19 -0400

    implementing redux in part, need to replace props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Profile from './components/profile';
+import store from './redux/store';
 
 
 function App() {
@@ -28,6 +29,9 @@ function App() {
         console.log(data);
         setToken(null);
         setProfile(null);
+        store.dispatch({
+          type: 'profile/logout'
+        })
       })
   }
 
@@ -35,11 +39,42 @@ function App() {
     window.location.href = 'http://localhost:3001/auth/spotify';
   }
 
+  function dispatchLogin() {
+    if (profile) {
+      store.dispatch({
+        type: 'profile/login',
+        payload: {
+          displayName: profile.displayName,
+          username: profile.id,
+          id: profile.id
+        }
+      })
+      console.log(store.getState())
+    }}
+
+  function dispatchToken() {
+    if (token) {
+      store.dispatch({
+        type: 'token/login',
+        payload: {
+          token: token
+        }
+      })
+      console.log(store.getState())
+    }
+  }
+
+  useEffect(() => {
+    dispatchLogin();
+    dispatchToken();
+  }, [profile]);
+
   return (
     <div className="App">
       <div classname="navbar navbar-inverse navbar-fixed-top" id='nav'>
         <button className='btn' onClick={handleLogin}>Login with Spotify</button>
         <button className='btn' onClick={handleLogout}>Logout</button>
+        <button className='btn' onClick={dispatchLogin}>Update store</button>
       </div>
       <div className="jumbotron"><h1>Recommendify</h1></div>
       { profile &&
@@ -47,6 +82,17 @@ function App() {
         <Profile profile={profile} token={token} />
       </div>
        }
+      {
+        store.getState().profile.displayName &&
+        <div>
+          <h1>Redux</h1>
+          <p>Display name: {store.getState().profile.displayName}</p>
+          <p>Username: {store.getState().profile.username}</p>
+          <p>ID: {store.getState().profile.id}</p>
+          <p>Token: {store.getState().token.token}</p>
+        </div>
+      }
+
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,44 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+function profileReducer(state = { 
+    displayName: 'John Doe',
+    username: 'johndoe',
+    id: '1234567890',
+ }, action) { 
+    switch (action.type) {
+        case 'profile/login':
+            return { ...state, displayName: action.payload.displayName, username: action.payload.username, id: action.payload.id }
+        case 'profile/logout':
+            return { ...state, 
+                displayName: 'John Doe',
+                username: 'johndoe',
+                id: '1234567890' }
+        default:
+            return state
+    }
+}
+
+function tokenReducer(state = { token: null }, action) {
+    switch (action.type) {
+        case 'token/login':
+            return { ...state, token: action.payload.token }
+        case 'token/logout':
+            return { ...state, token: null }
+        default:
+            return state
+    }
+}
+
 export default configureStore({
-    reducer: {}
+    reducer: {
+        profile: profileReducer,
+        token: tokenReducer
+    }
+})
+
+export const store = configureStore({
+    reducer: {
+        profile: profileReducer,
+    }
 })
 
