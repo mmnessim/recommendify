7af5407ae539236dc1858e9a5ff1ea475504e22f
Mounir Nessim <mnessim@ad.wcpss.net>
2023-09-19 09:59:31 -0400

    implement hooks in profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,23 +1,27 @@
 import React from "react";
 import Recommendations from "./recommendations";
 import PlaylistContainer from "./playlistContainer";
+import { useSelector } from "react-redux";
 
 export default function Profile(props) {
+    const profile = useSelector((state) => state.profile);
+    const token = useSelector((state) => state.token.token);
+
     return (
         <div className="container">
             <div className="row">
-                <h1>{props.profile.displayName}</h1>
+                <h1>{profile.displayName}</h1>
                 {
-                    props.profile.images &&
-                    <img src={props.profile.images[0].url} alt="profile pic" />
+                    profile.images &&
+                    <img src={profile.images[0].url} alt="profile pic" />
                 }
             </div>
             <div className="row">
                 <div className="col-sm">
-                    <Recommendations token={props.token} />
+                    <Recommendations token={token} />
                 </div>
                 <div className="col-sm">
-                    <PlaylistContainer token={props.token} profile={props.profile}/>
+                    <PlaylistContainer token={token} profile={profile}/>
                 </div>
             </div>
 
