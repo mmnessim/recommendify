3360333a9d1575f95ecaf927579f5c20b0a6f747
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-09-19 16:41:07 -0400

    Primitive add to playlist

--- src/components/fetchRec.js
+++ src/components/fetchRec.js
@@ -8,6 +8,23 @@ export default function FetchRec(props) {
     const [display, setDisplay] = useState([]);
 
     const token = useSelector((state) => state.token.token);
+    const playlistID = useSelector((state) => state.playlistID.playlistID);
+
+    function addToPlaylist(song) {
+        fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
+            method: 'POST',
+            headers: {
+                Authorization: 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                uris: [song.uri]
+            })
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+    }
 
     useEffect(() => {
         fetch('https://api.spotify.com/v1/recommendations?seed_artists=' + props.artistID, {
@@ -32,6 +49,8 @@ export default function FetchRec(props) {
                         <img src={rec.album.images[0].url} alt="album art" className='pic-sm' />
                         <audio controls src={rec.preview_url} />
                         <p>{rec.artists[0].name}</p>
+                        <br />
+                        <button className='btn' onClick={() => addToPlaylist(rec)}>Add to Selected Playlist</button>
                         <SongDetails song={rec} token={token} />
                     </div>
                 )
